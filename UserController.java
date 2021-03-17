58a9ad8ca0f87d22be56efa68b0b612ab68c7f7c
kimsy630 <kimsy663300@gmail.com>
2021-03-17 10:49:45 +0900

    Add UserConttroller[Login]

--- UserController.java
+++ UserController.java
@@ -6,4 +6,10 @@ public class UserController{
 		logger.Info("경로 : login");
 		retunr "user/login";
 	}
+	//회원가입 페이지로 이동
+	@RequestMapping("/user/insert.do")
+	public String insert(){
+		logger.Info("경로 : insert");
+		retunr "user/insert";
+	}
 }
